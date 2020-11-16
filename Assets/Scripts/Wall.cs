0b829e1ee17c9b5dd30fe554252d2d3df51004c1
Aleksey Papushin <aleks.papushin@gmail.com>
2020-11-17 00:40:18 +0300

    Added all the wall heating sprites, added behavior

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -6,10 +6,18 @@ public class Wall : MonoBehaviour
     public bool IsDangerous { get; private set; }
     public bool IsPlayerStandingOn { get; set; }
 
+    public Sprite safe;
+    public Sprite warning;
+    public Sprite danger;
+
+    private SpriteRenderer _spriteRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
-        this.ChangeColorTo(Color.green);
+        //this.ChangeColorTo(Color.green);
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.sprite = safe;
         
         IsDangerous = false;        
     }
@@ -22,23 +30,27 @@ public class Wall : MonoBehaviour
 
     public void BecameDangerous()
     {
-        this.ChangeColorTo(Color.red);
+        //this.ChangeColorTo(Color.red);
+        _spriteRenderer.sprite = danger;
         IsDangerous = true;
     }
 
     public IEnumerator BecameDangerousCoroutine(float _wallWarningInterval)
     {
-        this.ChangeColorTo(new Color(255/255f, 128/255f, 0/255f));
+        //this.ChangeColorTo(new Color(255/255f, 128/255f, 0/255f));
+        _spriteRenderer.sprite = warning;
 
         yield return new WaitForSeconds(_wallWarningInterval);
 
-        this.ChangeColorTo(Color.red);
+        //this.ChangeColorTo(Color.red);
+        _spriteRenderer.sprite = danger;
         IsDangerous = true;
     }
 
     public void BecameSafe()
     {
-        this.ChangeColorTo(Color.green);
+        //this.ChangeColorTo(Color.green);
+        _spriteRenderer.sprite = safe;
         IsDangerous = false;
     }
 
