9e9b5fd633215f10258a176c42e819841aa26f20
Aleksey Papushin <aleks.papushin@gmail.com>
2020-12-13 05:00:03 +0300

    Added logic to light walls according to state

--- Assets/Scripts/WallDanger.cs
+++ Assets/Scripts/WallDanger.cs
@@ -11,11 +11,14 @@ public class WallDanger : MonoBehaviour
     public Sprite danger;
 
     private SpriteRenderer _spriteRenderer;
+    private WallLighting _lighting;
 
     void Start()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _spriteRenderer.sprite = safe;
+
+        _lighting = GetComponentInChildren<WallLighting>(true);
         
         IsDangerous = false;        
     }
@@ -23,16 +26,19 @@ public class WallDanger : MonoBehaviour
     public IEnumerator BecameDangerousCoroutine(float _wallWarningInterval)
     {
         _spriteRenderer.sprite = warning;
+        _lighting.SetWarning();
 
         yield return new WaitForSeconds(_wallWarningInterval);
 
         _spriteRenderer.sprite = danger;
+        _lighting.SetDanger();
         IsDangerous = true;
     }
 
     public void BecameSafe()
     {
         _spriteRenderer.sprite = safe;
+        _lighting.Disable();
         IsDangerous = false;
     }
 }
---
+++ Assets/Scripts/WallLighting.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallLighting : MonoBehaviour
+{
+    [SerializeField] private int _dangerIntensity;
+    [SerializeField] private int _warningIntensity;
+
+    private Light _light;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _light = GetComponent<Light>();
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetWarning()
+    {
+        _light.intensity = _warningIntensity;
+        gameObject.SetActive(true);
+    }
+
+    public void SetDanger()
+    {
+        _light.intensity = _dangerIntensity;
+    }
+
+    public void Disable()
+    {
+        gameObject.SetActive(false);
+    }
+}
