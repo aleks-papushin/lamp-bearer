e683fab02eb5a866426b8cce584a120ac593de09
Aleksey Papushin <aleks.papushin@gmail.com>
2021-01-10 07:26:03 +0300

    Added audio source, music files, scripts for playing music

--- Assets/Scripts/SoundSource.cs
+++ Assets/Scripts/SoundSource.cs
@@ -5,6 +5,13 @@ namespace Assets.Scripts
 {
     public abstract class SoundSource : MonoBehaviour, ISoundSource
     {
+        public void PlayLoop(AudioSource source, AudioClip clip)
+        {
+            source.clip = clip;
+            source.loop = true;
+            source.Play();
+        }
+
         public void PlayOnce(AudioSource source, AudioClip clip)
         {
             source.PlayOneShot(clip);
--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -1,10 +1,13 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class StartButton : MonoBehaviour
 {
     private Button _startButton;
+
+    public static event Action OnGameStarted;
 
     private void Start()
     {
@@ -15,5 +18,6 @@ public class StartButton : MonoBehaviour
     private void StartGame()
     {
         SceneManager.LoadScene(1);
+        OnGameStarted?.Invoke();
     }
 }
