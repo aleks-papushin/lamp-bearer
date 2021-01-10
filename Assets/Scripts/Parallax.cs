3866bfafcafb5eeee17029d08479abc731c2f231
Aleksey Papushin <aleks.papushin@gmail.com>
2021-01-10 04:19:45 +0300

    Added background sprites, added parallax script to both backgrounds, a bit tuned

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,21 @@
+﻿using Assets.Scripts.Resources;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] private float _degree;
+    private GameObject _source;
+
+    void Start()
+    {
+        _source = GameObject.FindGameObjectWithTag(Tags.Player);                
+    }
+
+    void Update()
+    {
+        var newPosX = _source.transform.position.x * _degree;
+        var newPosY = _source.transform.position.y * _degree;
+
+        transform.position = new Vector3(newPosX, newPosY, transform.position.z);
+    }
+}
