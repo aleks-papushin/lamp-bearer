8b7d6b685103bb9411ab563c01af6946774c2396
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2021-03-15 11:12:21 +0100

    Get rid of specialized class for CSV reading

--- Assets/Scripts/Game/GameWaveManager.cs
+++ Assets/Scripts/Game/GameWaveManager.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using Enums;
+using Resources;
 
 namespace Game
 {
@@ -17,7 +20,7 @@ namespace Game
 
         public GameWaveManager()
         {
-            _waveList = new GameWaveCsvReader().ReadGameData(GameDifficulty);
+            _waveList = ReadGameData(GameDifficulty);
             CurrentNumber = _waveList.Min(w => w.number);
             _maxWaveNumber = _waveList.Max(w => w.number);
 
@@ -35,6 +38,44 @@ namespace Game
             {
                 CurrentNumber++;
             }
+        }
+        public List<GameWaveDto> ReadGameData(GameDifficulty difficulty)
+        {
+            var waveFilePath = difficulty == GameDifficulty.Easy
+                ? Paths.WaveEasyModeFilePath
+                : Paths.WaveHardModeFilePath;
+
+            using (var file = new StreamReader(waveFilePath))
+            {
+                _ = file.ReadLine(); // feed line with column names
+                string wave;
+                var gameWaves = new List<GameWaveDto>();
+
+                while ((wave = file.ReadLine()) != null)
+                {
+                    gameWaves.Add(ParseWave(wave));
+                }
+
+                return gameWaves;
+            }
+        }
+
+        private static GameWaveDto ParseWave(string waveString)
+        {
+            var wave = waveString.Split(',');
+
+            return new GameWaveDto
+            {
+                number = int.Parse(wave[0]),
+                wallWarningInterval = float.Parse(wave[1], CultureInfo.InvariantCulture),
+                wallDangerousInterval = float.Parse(wave[2], CultureInfo.InvariantCulture),
+                wallCoolDownInterval = float.Parse(wave[3], CultureInfo.InvariantCulture),
+                switchOnWalls = int.Parse(wave[4]) != 0,
+                enemyCount = int.Parse(wave[5]),
+                enemySpeed = float.Parse(wave[6], CultureInfo.InvariantCulture),
+                isOilAffectLight = int.Parse(wave[7]) != 0,
+                waveDuration = float.Parse(wave[8], CultureInfo.InvariantCulture)
+            };
         }
     }
 }
