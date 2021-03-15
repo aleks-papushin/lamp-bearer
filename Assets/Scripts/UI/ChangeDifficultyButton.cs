dc1105760ff01b01a96d6352575ea6bf0f3df847
Aleksey Papushin <aleks.papushin@gmail.com>
2021-03-15 23:14:17 +0300

    Added gamepade support for starting game, a bit refactored ChnageDifficultyButton

--- Assets/Scripts/UI/ChangeDifficultyButton.cs
+++ Assets/Scripts/UI/ChangeDifficultyButton.cs
@@ -2,26 +2,28 @@ using Enums;
 using Game;
 using TMPro;
 using UnityEngine;
-using Button = UnityEngine.UI.Button;
+using UnityEngine.UI;
 
 namespace UI
 {
-    public class ChangeDifficultyButton: MonoBehaviour
+    public class ChangeDifficultyButton : MonoBehaviour
     {
         private Button _changeDifficultyButton;
         public TMP_Text difficultyLabel;
+
+        public string DifficultyLabelText => $"{GameWaveManager.GameDifficulty} Mode";
 
         private void Start()
         {
             _changeDifficultyButton = GetComponent<Button>();
             _changeDifficultyButton.onClick.AddListener(ChangeDifficulty);
-            difficultyLabel.text = GameWaveManager.GameDifficulty + " Mode";
+            difficultyLabel.text = DifficultyLabelText;
         }
 
         private void ChangeDifficulty()
         {
             GameWaveManager.GameDifficulty = GameWaveManager.GameDifficulty == GameDifficulty.Easy ? GameDifficulty.Hard : GameDifficulty.Easy;
-            difficultyLabel.text = GameWaveManager.GameDifficulty + " Mode";
+            difficultyLabel.text = DifficultyLabelText;
         }
         
         private void Update()
