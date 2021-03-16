dc0dedf50f661f5ff4b9513ed7f9cc3882932525
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2021-03-16 23:47:26 +0100

    Simplify code

--- Assets/Scripts/HandleObjectFacing.cs
+++ Assets/Scripts/HandleObjectFacing.cs
@@ -73,10 +73,7 @@ public class HandleObjectFacing : MonoBehaviour
 
     private void FlipScale()
     {
-        var cachedTransform = transform;
-        var localScale = cachedTransform.localScale;
-        var newScale = new Vector3(localScale.x * -1, localScale.y, localScale.z);
-        localScale = newScale;
-        cachedTransform.localScale = localScale;
+        var localScale = transform.localScale;
+        transform.localScale = new Vector3(localScale.x * -1, localScale.y, localScale.z);
     }
 }
