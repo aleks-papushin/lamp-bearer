25bb24fffe2b05a409f140576f8066265b1e0c8a
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2021-03-17 00:28:42 +0100

    Fixup namespaces

--- Assets/Scripts/Player/JumpOverCorner.cs
+++ Assets/Scripts/Player/JumpOverCorner.cs
@@ -1,128 +1,130 @@
 ﻿using System;
-using Player;
 using Resources;
 using UnityEngine;
 using Wall;
 
-public class JumpOverCorner : MonoBehaviour
+namespace Player
 {
-    private enum Corner
+    public class JumpOverCorner : MonoBehaviour
     {
-        BottomLeft,
-        BottomRight,
-        UpperLeft,
-        UpperRight
-    }
+        private enum Corner
+        {
+            BottomLeft,
+            BottomRight,
+            UpperLeft,
+            UpperRight
+        }
     
-    [SerializeField] private float _cornerJumpForce;
-    [SerializeField] private float _cornerJumpModifier;
+        [SerializeField] private float _cornerJumpForce;
+        [SerializeField] private float _cornerJumpModifier;
 
-    private Rigidbody2D _rig;
+        private Rigidbody2D _rig;
 
-    private ObjectWallCollisions _wallCollisions;
-    private GravityHandler _gravitySwitcher;
-    private PlayerSounds _sound;
+        private ObjectWallCollisions _wallCollisions;
+        private GravityHandler _gravitySwitcher;
+        private PlayerSounds _sound;
 
-    private void Awake()
-    {
-        _rig = GetComponent<Rigidbody2D>();
-        _wallCollisions = GetComponent<ObjectWallCollisions>();
-        _gravitySwitcher = GetComponent<GravityHandler>();
-        _sound = GetComponent<PlayerSounds>();
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (!collision.gameObject.tag.Contains(Tags.CornerSuffix)) return;
-        switch (collision.gameObject.tag)
+        private void Awake()
         {
-            case Tags.BottomLeftCorner:
-                CornerJump(Corner.BottomLeft);
-                break;
-            case Tags.BottomRightCorner:
-                CornerJump(Corner.BottomRight);
-                break;
-            case Tags.UpperLeftCorner:
-                CornerJump(Corner.UpperLeft);
-                break;
-            case Tags.UpperRightCorner:
-                CornerJump(Corner.UpperRight);
-                break;
-            default:
-                throw new ArgumentOutOfRangeException();
+            _rig = GetComponent<Rigidbody2D>();
+            _wallCollisions = GetComponent<ObjectWallCollisions>();
+            _gravitySwitcher = GetComponent<GravityHandler>();
+            _sound = GetComponent<PlayerSounds>();
         }
-    }
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag.Contains(Tags.CornerSuffix))
+        private void OnTriggerEnter2D(Collider2D collision)
         {
+            if (!collision.gameObject.tag.Contains(Tags.CornerSuffix)) return;
+            switch (collision.gameObject.tag)
+            {
+                case Tags.BottomLeftCorner:
+                    CornerJump(Corner.BottomLeft);
+                    break;
+                case Tags.BottomRightCorner:
+                    CornerJump(Corner.BottomRight);
+                    break;
+                case Tags.UpperLeftCorner:
+                    CornerJump(Corner.UpperLeft);
+                    break;
+                case Tags.UpperRightCorner:
+                    CornerJump(Corner.UpperRight);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
-    }
 
-    private void CornerJump(Corner currentCorner)
-    {
-        switch (currentCorner)
+        private void OnTriggerExit2D(Collider2D collision)
         {
-            case Corner.BottomLeft:
-                if (_wallCollisions.IsTouchHorizontalWall)
-                {
-                    PerformCornerJump(
-                        new Vector2(-_cornerJumpForce * _cornerJumpModifier, _cornerJumpForce), Direction.Left);
-                }
-                else if (_wallCollisions.IsTouchVerticalWall)
-                {
-                    PerformCornerJump(
-                        new Vector2(_cornerJumpForce, -_cornerJumpForce * _cornerJumpModifier), Direction.Down);
-                }
-                break;
-            case Corner.BottomRight:
-                if (_wallCollisions.IsTouchHorizontalWall)
-                {
-                    PerformCornerJump(
-                        new Vector2(_cornerJumpForce * _cornerJumpModifier, _cornerJumpForce), Direction.Right);
-                }
-                else if (_wallCollisions.IsTouchVerticalWall)
-                {
-                    PerformCornerJump(
-                        new Vector2(-_cornerJumpForce, -_cornerJumpForce * _cornerJumpModifier), Direction.Down);
-                }
-                break;
-            case Corner.UpperLeft:
-                if (_wallCollisions.IsTouchHorizontalWall)
-                {
-                    PerformCornerJump(
-                        new Vector2(-_cornerJumpForce * _cornerJumpModifier, -_cornerJumpForce), Direction.Left);
-                }
-                else if (_wallCollisions.IsTouchVerticalWall)
-                {
-                    PerformCornerJump(
-                        new Vector2(_cornerJumpForce, _cornerJumpForce * _cornerJumpModifier), Direction.Up);
-                }
-                break;
-            case Corner.UpperRight:
-                if (_wallCollisions.IsTouchHorizontalWall)
-                {
-                    PerformCornerJump(
-                        new Vector2(_cornerJumpForce * _cornerJumpModifier, -_cornerJumpForce), Direction.Right);
-                }
-                else if (_wallCollisions.IsTouchVerticalWall)
-                {
-                    PerformCornerJump(
-                        new Vector2(-_cornerJumpForce, _cornerJumpForce * _cornerJumpModifier), Direction.Up);
-                }
-                break;
-            default:
-                throw new ArgumentOutOfRangeException(nameof(currentCorner), currentCorner, null);
+            if (collision.gameObject.tag.Contains(Tags.CornerSuffix))
+            {
+            }
         }
-    }
 
-    private void PerformCornerJump(Vector2 force, Direction newGravity)
-    {
-        _rig.velocity = Vector2.zero;
-        _rig.AddForce(force, ForceMode2D.Impulse);
-        _gravitySwitcher.SwitchLocalGravity(newGravity);
-        _sound.CornerJump();
+        private void CornerJump(Corner currentCorner)
+        {
+            switch (currentCorner)
+            {
+                case Corner.BottomLeft:
+                    if (_wallCollisions.IsTouchHorizontalWall)
+                    {
+                        PerformCornerJump(
+                            new Vector2(-_cornerJumpForce * _cornerJumpModifier, _cornerJumpForce), Direction.Left);
+                    }
+                    else if (_wallCollisions.IsTouchVerticalWall)
+                    {
+                        PerformCornerJump(
+                            new Vector2(_cornerJumpForce, -_cornerJumpForce * _cornerJumpModifier), Direction.Down);
+                    }
+                    break;
+                case Corner.BottomRight:
+                    if (_wallCollisions.IsTouchHorizontalWall)
+                    {
+                        PerformCornerJump(
+                            new Vector2(_cornerJumpForce * _cornerJumpModifier, _cornerJumpForce), Direction.Right);
+                    }
+                    else if (_wallCollisions.IsTouchVerticalWall)
+                    {
+                        PerformCornerJump(
+                            new Vector2(-_cornerJumpForce, -_cornerJumpForce * _cornerJumpModifier), Direction.Down);
+                    }
+                    break;
+                case Corner.UpperLeft:
+                    if (_wallCollisions.IsTouchHorizontalWall)
+                    {
+                        PerformCornerJump(
+                            new Vector2(-_cornerJumpForce * _cornerJumpModifier, -_cornerJumpForce), Direction.Left);
+                    }
+                    else if (_wallCollisions.IsTouchVerticalWall)
+                    {
+                        PerformCornerJump(
+                            new Vector2(_cornerJumpForce, _cornerJumpForce * _cornerJumpModifier), Direction.Up);
+                    }
+                    break;
+                case Corner.UpperRight:
+                    if (_wallCollisions.IsTouchHorizontalWall)
+                    {
+                        PerformCornerJump(
+                            new Vector2(_cornerJumpForce * _cornerJumpModifier, -_cornerJumpForce), Direction.Right);
+                    }
+                    else if (_wallCollisions.IsTouchVerticalWall)
+                    {
+                        PerformCornerJump(
+                            new Vector2(-_cornerJumpForce, _cornerJumpForce * _cornerJumpModifier), Direction.Up);
+                    }
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(currentCorner), currentCorner, null);
+            }
+        }
+
+        private void PerformCornerJump(Vector2 force, Direction newGravity)
+        {
+            _rig.velocity = Vector2.zero;
+            _rig.AddForce(force, ForceMode2D.Impulse);
+            _gravitySwitcher.SwitchLocalGravity(newGravity);
+            _sound.CornerJump();
+        }
     }
 }
 
