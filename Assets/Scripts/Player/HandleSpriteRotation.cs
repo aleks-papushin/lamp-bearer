aaa2d744d49aa0b1e08956eacf71b36f1173a3c7
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2021-03-17 00:28:42 +0100

    Fixup namespaces

--- Assets/Scripts/Player/HandleSpriteRotation.cs
+++ Assets/Scripts/Player/HandleSpriteRotation.cs
@@ -4,60 +4,63 @@ using UnityEngine;
 using Utils;
 using Wall;
 
-public class HandleSpriteRotation : MonoBehaviour
+namespace Player
 {
-    [SerializeField] private float _startRotationDistance;
-    [SerializeField] private float _defaultRotationSpeed;
-    private float _rotationSpeed;
-
-    private GravityHandler _gravityHandler;
-    private ObjectWallCollisions _groundedStateHandler;
-
-    private GameObject Ground => DirectionUtils.GetFloorFor(_gravityHandler.GravityVector);
-
-    private void Awake()
+    public class HandleSpriteRotation : MonoBehaviour
     {
-        _rotationSpeed = _defaultRotationSpeed;
-        _groundedStateHandler = GetComponent<ObjectWallCollisions>();
-        _gravityHandler = GetComponent<GravityHandler>();
-    }
+        [SerializeField] private float _startRotationDistance;
+        [SerializeField] private float _defaultRotationSpeed;
+        private float _rotationSpeed;
 
-    private void FixedUpdate()
-    {
-        Handle();
-    }
+        private GravityHandler _gravityHandler;
+        private ObjectWallCollisions _groundedStateHandler;
 
-    private void Handle()
-    {
-        if (_groundedStateHandler.IsGrounded) return;
+        private GameObject Ground => DirectionUtils.GetFloorFor(_gravityHandler.GravityVector);
 
-        var ground = DirectionUtils.GetFloorFor(_gravityHandler.GravityVector);
-        var distanceToGround = transform.GetDistanceTo(ground);
-        if (distanceToGround < _startRotationDistance)
-        {
-            RotateTowards(_gravityHandler.GravityVector, true);
-        }
-        else
+        private void Awake()
         {
             _rotationSpeed = _defaultRotationSpeed;
-            RotateTowards(DirectionUtils.OppositeTo(_gravityHandler.GravityVector), false);
+            _groundedStateHandler = GetComponent<ObjectWallCollisions>();
+            _gravityHandler = GetComponent<GravityHandler>();
         }
-    }
 
-    private void RotateTowards(Direction gravityVector, bool accelerateRotation)
-    {
-        if (accelerateRotation)
+        private void FixedUpdate()
         {
-            // 4 is magic number which is should be bigger than any GetDistanceTo(Ground)
-            var modifier = 4 / transform.GetDistanceTo(Ground);
-            _rotationSpeed *= modifier;
+            Handle();
         }
 
-        var floorAnchor = DirectionUtils.GetFloorFor(gravityVector)
-            .GetComponentsInChildren<Transform>()
-            .Single(t => t.tag.Contains(Tags.AnchorSuffix));
+        private void Handle()
+        {
+            if (_groundedStateHandler.IsGrounded) return;
 
-        transform.rotation =
-            Quaternion.RotateTowards(transform.rotation, floorAnchor.transform.rotation, Time.deltaTime * _rotationSpeed);
+            var ground = DirectionUtils.GetFloorFor(_gravityHandler.GravityVector);
+            var distanceToGround = transform.GetDistanceTo(ground);
+            if (distanceToGround < _startRotationDistance)
+            {
+                RotateTowards(_gravityHandler.GravityVector, true);
+            }
+            else
+            {
+                _rotationSpeed = _defaultRotationSpeed;
+                RotateTowards(DirectionUtils.OppositeTo(_gravityHandler.GravityVector), false);
+            }
+        }
+
+        private void RotateTowards(Direction gravityVector, bool accelerateRotation)
+        {
+            if (accelerateRotation)
+            {
+                // 4 is magic number which is should be bigger than any GetDistanceTo(Ground)
+                var modifier = 4 / transform.GetDistanceTo(Ground);
+                _rotationSpeed *= modifier;
+            }
+
+            var floorAnchor = DirectionUtils.GetFloorFor(gravityVector)
+                .GetComponentsInChildren<Transform>()
+                .Single(t => t.tag.Contains(Tags.AnchorSuffix));
+
+            transform.rotation =
+                Quaternion.RotateTowards(transform.rotation, floorAnchor.transform.rotation, Time.deltaTime * _rotationSpeed);
+        }
     }
 }
