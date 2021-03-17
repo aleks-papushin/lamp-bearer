3924162ffaa01c46a181034972675488a7d21bd0
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2021-03-17 21:35:54 +0100

    Make SpawnEnemy simple

--- Assets/Scripts/Spawn/SpawnEnemy.cs
+++ Assets/Scripts/Spawn/SpawnEnemy.cs
@@ -8,38 +8,18 @@ namespace Spawn
     {
         [SerializeField] private GameObject _enemyPrefab;
         [SerializeField] private bool _isEnemyDirectionPositive;
-        [SerializeField] private Direction _initGravity;
         [SerializeField] private GameObject _pairSpawner;
-        private GameWaveManager _gameWaveManager;
+        private GameObject _enemy;
 
-        private GameObject Enemy { get; set; }
-        public int EnemyCount => Enemy != null ? 1 : 0;
-        public int PairEnemyCount => EnemyCount + _pairSpawner.GetComponent<SpawnEnemy>().EnemyCount;
-
-
-        private void Awake()
-        {
-            _gameWaveManager = FindObjectOfType<GameWaveManager>();
-            Enemy = null;
-        }
-
-        public void Spawn()
+        public int EnemyCount => _enemy != null ? 1 : 0;
+        public bool IsFree => EnemyCount + _pairSpawner.GetComponent<SpawnEnemy>().EnemyCount == 0;
+        
+        public void Spawn(float speed)
         {
             var cachedTransform = transform;
-            Enemy = Instantiate(_enemyPrefab, cachedTransform.position, cachedTransform.rotation);
-            SetInitDirection(Enemy);
-            SetSpeed(Enemy);
-        }
-
-        private void SetSpeed(GameObject enemy)
-        {
-            var speed = _gameWaveManager.CurrentWave.enemySpeed;
-            enemy.GetComponent<EnemyWalkerMovement>().Speed = speed;
-        }
-
-        private void SetInitDirection(GameObject enemy)
-        {
-            enemy.GetComponent<EnemyWalkerMovement>().IsDirectionPositive = _isEnemyDirectionPositive;
+            _enemy = Instantiate(_enemyPrefab, cachedTransform.position, cachedTransform.rotation);
+            _enemy.GetComponent<EnemyWalkerMovement>().IsDirectionPositive = _isEnemyDirectionPositive;
+            _enemy.GetComponent<EnemyWalkerMovement>().Speed = speed;
         }
     }
 }
