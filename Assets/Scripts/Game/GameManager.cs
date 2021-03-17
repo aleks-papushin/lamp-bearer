fcf23cba8fba32c52426bce96a9c74ce85a73d97
Aleksey Papushin <aleks.papushin@gmail.com>
2021-03-18 00:56:38 +0300

    Forgot to add file after conflict fixing

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -11,22 +11,25 @@ namespace Game
 {
     public class GameManager : MonoBehaviour
     {
+        [SerializeField] private float _wallWarningInterval;
+        [SerializeField] private float _wallDangerousInterval;
+        [SerializeField] private float _wallCoolDownInterval;
+
         public GameObject _spawner;
         public int oilBottleCountForSpawn;
 
         private GameWaveManager _waveManager;
         private Score _score;
-        private static List<GameObject> WallsToBeDangerous => FindObjectsOfType<WallDanger>().Select(w => w.gameObject).ToList();
 
-        [SerializeField] private float _wallWarningInterval;
-        [SerializeField] private float _wallDangerousInterval;
-        [SerializeField] private float _wallCoolDownInterval;
-        
         public int CurrentScore { get; private set; }
+
+        private static List<GameObject> WallsToBeDangerous { get; set; }
+        private static List<GameObject> RestingWalls { get; set; } = new List<GameObject>();        
 
         private void Awake()
         {
             _waveManager = FindObjectOfType<GameWaveManager>();
+            WallsToBeDangerous = FindObjectsOfType<WallDanger>().Select(w => w.gameObject).ToList();
         }
 
         private void Start()
@@ -59,23 +62,37 @@ namespace Game
             {
                 yield return null;
 
-                while (!_waveManager.CurrentWave.switchOnWalls)
+                while (_waveManager.CurrentWave.dangerWallAmount == 0)
                 {
                     yield return new WaitForSeconds(1);
                 }
 
                 var dangerousInterval = _wallDangerousInterval + _wallWarningInterval;
-
-                var wall = WallsToBeDangerous[new Random().Next(WallsToBeDangerous.Count)];
-                
+                var wall = WallsToBeDangerous[new Random().Next(WallsToBeDangerous.Count)];                
                 StartCoroutine(wall.GetComponent<WallDanger>().BecameDangerousCoroutine(_wallWarningInterval));
 
                 yield return new WaitForSeconds(dangerousInterval);
 
                 wall.GetComponent<WallDanger>().BecameSafe();
+                MoveRestingWallsToDangerous();
+                MoveWallToRestingWalls(wall);
 
                 yield return new WaitForSeconds(_wallCoolDownInterval);
             }
+        }
+
+        private void MoveRestingWallsToDangerous()
+        {
+            if (RestingWalls.Count == 0) return;
+
+            WallsToBeDangerous.AddRange(RestingWalls);
+            RestingWalls.Clear();
+        }
+
+        private void MoveWallToRestingWalls(GameObject wall)
+        {
+            WallsToBeDangerous.Remove(wall);
+            RestingWalls.Add(wall);
         }
     }
 }
