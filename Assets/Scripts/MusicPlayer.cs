2616a5f7aec2c03d349e8d3ff785476dfa8304d1
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2021-03-22 01:20:06 +0100

    Enable Music Player even if the GameScene is loaded first

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -12,7 +12,14 @@ public class MusicPlayer : MonoBehaviour
 
     private void Awake()
     {
-        DontDestroyOnLoad(gameObject);
+        if (FindObjectsOfType<MusicPlayer>().Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
     }
 
     private void Start()
