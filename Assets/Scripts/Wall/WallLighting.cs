3783f6f13f4eca0a39e16d0eb50628a7d5ba4191
aleksey.papushin <aleks.papushin@gmail.com>
2021-04-04 21:07:26 +0300

    Decreased wall brightness intensity and removed serialized attribute from wall warning intensity

--- Assets/Scripts/Wall/WallLighting.cs
+++ Assets/Scripts/Wall/WallLighting.cs
@@ -5,14 +5,14 @@ namespace Wall
     public class WallLighting : MonoBehaviour
     {
         [SerializeField] private int _dangerIntensity;
-        [SerializeField] private int _warningIntensity;
-
+        private int _warningIntensity;
         private Light _light;
 
         // Start is called before the first frame update
         private void Start()
         {
             _light = GetComponent<Light>();
+            _warningIntensity = (int)(_dangerIntensity * 0.5);
             gameObject.SetActive(false);
         }
 
